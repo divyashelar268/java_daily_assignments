92a45e58aebf4d3d1e955455ca84bd279391a0c0
divya <divyashelar268>
2024-08-28 19:50:22 +0530

    Day 3

---
+++ day3/assign1.java
@@ -0,0 +1,15 @@
+class Codex {
+		public static void main (String [] args){
+				for (int i=1; i<=4; i++) {
+				System.out.print ( "*  ");
+					
+				}
+
+
+
+
+
+
+
+		 }
+}
---
+++ day3/assign4.java
@@ -0,0 +1,15 @@
+class Demo {
+          public static void main (String [] args){
+		for (char i ='A' ; i<='D'; i++){
+				System.out.print(i+" ");
+				}
+
+
+
+
+
+
+
+
+    }
+}
