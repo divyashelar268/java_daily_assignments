5b4099bf2e04f8010ed52ff41df089b09bc8a17e
divya <divyashelar268>
2024-09-02 19:33:01 +0530

    Day5

---
+++ day5/assign2.java
@@ -0,0 +1,16 @@
+class Codex {
+		public static void main (String [] args){
+			int a =20;
+			int b=30;
+			int c =40;
+			if (a<=b && a<=c){
+				System.out.println ( a+ "is the smallest among of all numbers ");
+			}else if (b<=a && b<=c){
+				System.out.println (b+ "is the smallest among of all numbers  " );
+				}
+			else 
+				System.out.println (c+ " is the smallest among of all numbers " );
+
+
+		}
+}
