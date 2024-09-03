31f15c9516906a37f59599e27240b8727bad6b31
divya <divyashelar268>
2024-09-03 21:16:56 +0530

    Day6

---
+++ day6/assign1.java
@@ -0,0 +1,17 @@
+class Codex {
+		public static void main (String [] args){
+			int a=10;
+			int b=20;
+			int c =30;
+			if (a>=b || a>=c){
+				System.out.println(a+ "is the smallest number");
+			}else if (b>=a || b>=c){
+				System.out.println (b+ "is the second largest  number");
+			}else{
+				System.out.println (c+ " is the largest among of all numbers " );
+
+		}
+			
+ 
+	}
+}
---
+++ day6/assign6.java
@@ -0,0 +1,15 @@
+class DG	{
+		public static void main (String [] args){
+			for(int i=1; i<=4; i++){
+				char ch= 'A';
+				for(int j=1; j<=4; j++){
+					System.out.print(ch+ " ");
+					ch++;
+					
+				}
+			System.out.println();
+         }
+				
+
+	}
+}
