d25fcb9b51704c3289f4e28fcad141c6db66dda3
divya <divyashelar268>
2024-09-06 11:14:18 +0530

    Day 8

---
+++ day8/assign3.java
@@ -0,0 +1,12 @@
+class Divya {
+		public static void main(String [] args){
+		for(int i=1; i<=5; i++){
+			for(int j=1; j<i; j++){
+			 System.out.print ("01 ");
+		}
+			System.out.println();
+		}
+
+
+	}
+}
